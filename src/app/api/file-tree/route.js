5a29cc691c89f14768f3fd6bff6af43a51a74f2a
Jesús Mendoza <133887362+JessEleven@users.noreply.github.com>
2025-03-19 19:49:05 -0500

    API created to visualize the project file tree

---
+++ src/app/api/file-tree/route.js
@@ -0,0 +1,57 @@
+import apiResponse from '@/utils/api/api-response'
+import { promises as fs } from 'fs'
+import path from 'path'
+
+export async function GET (req) {
+  try {
+    // Directorio raíz del proyecto
+    const basePath = process.cwd()
+    // Solo se muestra 'src' como nodo raíz
+    const relativePath = 'src'
+    const fullPath = path.join(basePath, relativePath)
+
+    const tree = {
+      name: 'src',
+      type: 'folder',
+      path: relativePath,
+      file_tree: await getFileTree(fullPath, relativePath)
+    }
+    return apiResponse.success(
+      200,
+      'File tree fetched successfully',
+      tree
+    )
+  } catch (error) {
+    return apiResponse.error(
+      500,
+      'Error reading directory',
+      error
+    )
+  }
+}
+
+async function getFileTree (dirPath, relativePath) {
+  const entries = await fs.readdir(dirPath, { withFileTypes: true })
+
+  const folders = []
+  const files = []
+
+  for (const entry of entries) {
+    const entryPath = path.join(relativePath, entry.name)
+    if (entry.isDirectory()) {
+      folders.push({
+        name: entry.name,
+        type: 'folder',
+        path: entryPath,
+        content: await getFileTree(path.join(dirPath, entry.name), entryPath)
+      })
+    } else {
+      files.push({
+        name: entry.name,
+        type: 'file',
+        path: entryPath
+      })
+    }
+  }
+  return [...folders.sort((a, b) => a.name.localeCompare(b.name)), ...files.sort((a, b) => a.name.localeCompare(b.name))]
+}
