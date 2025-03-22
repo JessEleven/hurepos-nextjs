7d1a491979957e46a55b669fff52ce930d062179
Jesús Mendoza <133887362+JessEleven@users.noreply.github.com>
2025-03-21 23:43:50 -0500

    bug fix for deployment

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,6 @@
 import InfoCard from '@/components/cards/info-card'
 import SearchUser from '@/components/search-user'
-import { SearchContext } from '@/context/search-context'
+import { SearchContext } from '../context/search-context.jsx'
 
 export default function Home () {
   return (
